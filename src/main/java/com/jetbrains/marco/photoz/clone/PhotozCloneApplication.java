5a7836c06fa3a71d1a14d025ebad70ef1fe3482a
aman <Aman123@gmail.com>
2023-01-30 17:17:47 +0530

    code was commited

--- src/main/java/com/jetbrains/marco/photoz/clone/PhotozCloneApplication.java
+++ src/main/java/com/jetbrains/marco/photoz/clone/PhotozCloneApplication.java
@@ -6,16 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PhotozCloneApplication {
 
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(PhotozCloneApplication.class, args);
 		Photo photo = new Photo();
 		photo.setId(" 32 watts");
-		System.out.println("company  product id=" + photo.getId());
+		System.out.println("company  main  product id=" + photo.getId());
 	}
 
-	private static String setId1() {
+	public static String setId1() {
 
 
 		return null;
