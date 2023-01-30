ecd35c2bf8fd0ddb2bd47d4f97934a61b1376d7c
aman <Aman123@gmail.com>
2023-01-30 16:46:19 +0530

    commot

--- src/main/java/com/jetbrains/marco/photoz/clone/PhotozCloneApplication.java
+++ src/main/java/com/jetbrains/marco/photoz/clone/PhotozCloneApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PhotozCloneApplication {
 
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(PhotozCloneApplication.class, args);
 		Photo photo = new Photo();
